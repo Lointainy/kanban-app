b9957df65d269356ff89bfdcb0d85b8fc41da90e
Eugene_dev <lointainyyy@gmail.com>
2023-04-24 12:10:26 +0300

    refactor

--- server/controllers/columnController.js
+++ server/controllers/columnController.js
@@ -7,14 +7,14 @@ const createColumn = async (req, res) => {
 
 	try {
 		let userData = await Board.findOne({ user_id })
+
 		let board = userData.boards.find((b) => b._id == boardId)
+		!board && res.status(404).json({ msg: `No Board with id:${boardId}` })
 
 		if (board) {
 			board.columns.push({ ...newColumn, tasks: [] })
 			await userData.save()
 			res.status(200).json(`New Column in board are created`)
-		} else {
-			res.status(404).json({ msg: `No Board with id:${boardId}` })
 		}
 	} catch (error) {
 		res.status(500).json({ msg: error.message })
