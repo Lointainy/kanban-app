37fa0a9b04ebc39ede4595f323b70bf54856d56a
Eugene_dev <lointainyyy@gmail.com>
2023-04-24 12:09:29 +0300

    refactor

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -7,19 +7,17 @@ const createTask = async (req, res) => {
 
 	try {
 		let userData = await Board.findOne({ user_id })
-		let board = userData.boards.find((b) => b._id == boardId)
 
-		if (board) {
-			let column = board.columns.find((c) => c._id == columnId)
-			if (column) {
-				column.tasks.push({ ...newTask, subtasks: [] })
-				await userData.save()
-				res.status(200).json(`New Tasks in column are created`)
-			} else {
-				res.status(404).json({ msg: `No Column with id:${columnId}` })
-			}
-		} else {
-			res.status(404).json({ msg: `No Board with id:${boardId}` })
+		let board = userData.boards.find((b) => b._id == boardId)
+		!board && res.status(404).json({ msg: `No Board with id:${boardId}` })
+
+		let column = board.columns.find((c) => c._id == columnId)
+		!column && res.status(404).json({ msg: `No Column with id:${columnId}` })
+
+		if (column) {
+			column.tasks.push({ ...newTask, subtasks: [] })
+			await userData.save()
+			res.status(200).json(`New Tasks in column are created`)
 		}
 	} catch (error) {
 		res.status(500).json({ msg: error.message })
