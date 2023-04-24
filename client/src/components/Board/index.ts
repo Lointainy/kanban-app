89a1774cf2b1ae62c617dc5382a8c2d39f1c65ed
Eugene_dev <lointainyyy@gmail.com>
2023-04-24 23:19:14 +0300

    delete components

--- client/src/components/Board/index.ts
+++ client/src/components/Board/index.ts
@@ -1,5 +1,3 @@
 export { default as Column } from './Column/Column'
 export { default as Task } from './Task/Task'
-export { default as NewColumn } from './NewColumn/NewColumn'
-export { default as NewTask } from './NewTask/NewTask'
 export { default as CreateItemForm } from './CreateItemForm/CreateItemForm'
