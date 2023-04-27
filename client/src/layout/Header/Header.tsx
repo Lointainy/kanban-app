057bb54492864dbb79a606ad571f076c4cf1d069
Eugene_dev <lointainyyy@gmail.com>
2023-04-27 13:05:05 +0300

    refactore logic

--- client/src/layout/Header/Header.tsx
+++ client/src/layout/Header/Header.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from 'react'
 /* Router */
 import { NavLink, useLocation } from 'react-router-dom'
 
@@ -7,14 +6,14 @@ import { useAppDispatch, useAppSelector } from '@hooks/useRedux'
 import { logout } from '@store/reducers/authSlice'
 import { openModal } from '@store/reducers/modalSlice'
 
-/* Hooks */
-import { useToggle } from '@hooks/useToggle'
-
 /* Styles */
 import style from './Header.module.scss'
 
 /* Icons */
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+
+/* Components */
+import { DropdownOptions } from '@components'
 
 /* Logo */
 import darkLogo from '@/assets/images/logo/dark-theme-logo.svg'
@@ -32,43 +31,35 @@ const Header: React.FC = () => {
   // Location path
   const { pathname } = useLocation()
 
-  const { toggle: optionDropdown, handleToggle: optionDropdownToggle, setToggle: setOptionDropdown } = useToggle(false)
+  const options = [
+    pathname.includes('/board/') && {
+      label: 'Edit board',
+      onClick: () => handleClickOption('EditBoard'),
+    },
+    pathname.includes('/board/') && {
+      label: 'Delete board',
+      error: true,
+      onClick: () => handleClickOption('DeleteBoard'),
+    },
+    { label: 'Logout', error: true, onClick: () => handleClickOption('Logout') },
+  ]
 
-  const handleOption = (name: string) => {
+  const handleClickOption = (name: string) => {
     switch (name) {
       case 'AddNewTask':
         dispatch(openModal({ name: 'AddNewTask' }))
         break
       case 'EditBoard':
-        dispatch(openModal({ name: 'EditBoard', data: board }))
+        dispatch(openModal({ name: 'EditBoard' }))
         break
       case 'DeleteBoard':
         dispatch(openModal({ name: 'DeleteBoard' }))
         break
+      case 'Logout':
+        dispatch(logout())
+        break
     }
-
-    return setOptionDropdown(false)
   }
-
-  const handleLogout = () => {
-    dispatch(logout())
-    return setOptionDropdown(false)
-  }
-
-  const dropdownRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        setOptionDropdown(false)
-      }
-    }
-    document.addEventListener('mousedown', handleOutsideClick)
-
-    return () => {
-      document.removeEventListener('mousedown', handleOutsideClick)
-    }
-  }, [])
 
   return (
     <div className={style.header}>
@@ -81,36 +72,11 @@ const Header: React.FC = () => {
 
       <h1 className={style.title}>{pathname.includes('/board/') && board.name}</h1>
 
-      <button className={style.btn} onClick={() => handleOption('AddNewTask')}>
+      <button className={style.btn}>
         <Icon icon="plus" />
         <span>Add new task</span>
       </button>
-      <div className={style.option} ref={dropdownRef}>
-        <button className={style.button} onClick={optionDropdownToggle}>
-          <Icon icon="ellipsis-vertical" className={style.icon} />
-        </button>
-        {optionDropdown && (
-          <div className={style.option__dropdown} onClick={(e) => e.stopPropagation()}>
-            <ul className={style.option__dropdown_list}>
-              {pathname.includes('/board/') && (
-                <>
-                  <li className={style.option__dropdown_item} onClick={() => handleOption('EditBoard')}>
-                    Edit Board
-                  </li>
-                  <li
-                    className={`${style.option__dropdown_item} ${style.error}`}
-                    onClick={() => handleOption('DeleteBoard')}>
-                    Delete Board
-                  </li>
-                </>
-              )}
-              <li className={`${style.option__dropdown_item} ${style.error}`} onClick={() => handleLogout()}>
-                Logout
-              </li>
-            </ul>
-          </div>
-        )}
-      </div>
+      <DropdownOptions options={options} fieldStyle="invert" />
     </div>
   )
 }
