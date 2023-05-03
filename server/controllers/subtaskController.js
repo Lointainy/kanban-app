f6f4a67682123893848131a71a8e38dd9e06ddce
Eugene_dev <lointainyyy@gmail.com>
2023-05-03 12:52:24 +0300

    fixed logic

--- server/controllers/subtaskController.js
+++ server/controllers/subtaskController.js
@@ -9,22 +9,41 @@ const createSubtask = async (req, res) => {
 		let userData = await Board.findOne({ user_id })
 
 		let board = userData.boards.find((b) => b._id == boardId)
-		!board && res.status(404).json({ msg: `No Board with id:${boardId}` })
+		if (!board) res.status(404).json({ msg: `No Board with id:${boardId}` })
 
 		let column = board.columns.find((c) => c._id == columnId)
-		!column && res.status(404).json({ msg: `No Column with id:${columnId}` })
+		if (!column) res.status(404).json({ msg: `No Column with id:${columnId}` })
 
-		let task = column.tasks.find((t) => t._id == taskId)
-		!task && res.status(404).json({ msg: `No task with id:${taskId}` })
+		let task = column.tasks.find((c) => c._id == taskId)
+		if (!task) res.status(404).json({ msg: `No Task with id:${taskId}` })
 
-		if (task) {
-			task.subtasks.push({ ...newSubtask, isCompleted: false })
-			await userData.save()
-			res.status(200).json(`New Subtask in task are created`)
+		const filter = {
+			user_id,
+			'boards._id': boardId,
+			'boards.columns._id': columnId,
+			'boards.columns.tasks._id': taskId
 		}
+
+		const subtask = {
+			...newSubtask
+		}
+
+		const update = {
+			$push: {
+				'boards.$[board].columns.$[column].tasks.$[task].subtasks': subtask
+			}
+		}
+
+		const options = {
+			arrayFilters: [{ 'board._id': boardId }, { 'column._id': columnId }, { 'task._id': taskId }]
+		}
+
+		await Board.updateOne(filter, update, options)
+
+		res.status(200).json(`New subtask in task are created`)
 	} catch (error) {
-		console.error(error)
-		res.status(500).json({ msg: 'Internal Server Error' })
+		res.status(500).json({ msg: error.message })
+		console.log(error)
 	}
 }
 
